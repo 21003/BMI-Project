d49bb088fa01393bfa623a37cc2e943dd0799b6a
Jacob Johnston <jacob.johnston.2022@student.lhschools.org>
2019-02-14 12:50:26 -0500

    1-20 Asked name

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,7 +1,19 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        String Name;
+
+
+        System.out.println("Hello!!! What is you name ?");
+        Name=keyboard.nextLine();
+        System.out.println("Hello "+Name+"");
+
+
     }
 }
